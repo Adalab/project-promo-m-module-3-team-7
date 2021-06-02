9ee4db49582e091767aa14031a7f33a9325bec99
luciaenriquezbos <eboscreative@gmail.com>
2021-06-02 13:40:22 +0200

    add Collapsable

---
+++ project-team-7/src/components/Collapsable.js
@@ -0,0 +1,11 @@
+function Collapsable() {
+  return (
+    <div className="form__title js-collapsable-header collapsable--close">
+      <h2 className="form__text js-collapsable-header">
+        <i className="far fa-object-ungroup form__iconL"></i>Diseña
+      </h2>
+      <i className="fas fa-angle-up form__iconR"></i>
+    </div>
+  );
+}
+export default Collapsable;
