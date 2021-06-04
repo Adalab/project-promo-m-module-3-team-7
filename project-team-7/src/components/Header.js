199e09e72394eb39f6e08671d3d30e11cefc2ea0
Ana Salamero <asalamerosanz@gmail.com>
2021-06-04 13:21:14 +0200

    added landing component

--- project-team-7/src/components/Header.js
+++ project-team-7/src/components/Header.js
@@ -2,13 +2,13 @@ import logoHeader from '../images/logo-u.png';
 
 function Header() {
   return (
-    <header className='header-design-card'>
+    <header className="header-design-card">
       <img
         src={logoHeader}
-        className='header-design-card__img'
-        title='Awesome Profile Cards'
-        alt='Awesome Profile
-    Cards Logo'
+        className="header-design-card__img"
+        title="Awesome Profile Cards"
+        alt="Awesome Profile
+    Cards Logo"
       />
     </header>
   );
