4d6cab64343843c4b4b6623af37a72280ed3e3b0
Ana Salamero <asalamerosanz@gmail.com>
2021-06-04 13:38:42 +0200

    added icon as props

--- project-team-7/src/components/CollapsableHeader.js
+++ project-team-7/src/components/CollapsableHeader.js
@@ -2,7 +2,7 @@ function CollapsableHeader(props) {
   return (
     <div className="form__title js-collapsable-header collapsable--close">
       <h2 className="form__text js-collapsable-header">
-        <i className="far fa-object-ungroup form__iconL"></i>
+        <i className={props.icon}></i>
         {props.title}
       </h2>
       <i className="fas fa-angle-up form__iconR"></i>
--- project-team-7/src/components/Form.js
+++ project-team-7/src/components/Form.js
@@ -1,24 +1,30 @@
-import CollapsableHeader from "./CollapsableHeader";
-import Design from "./Design";
-import Fill from "./Fill";
-import Share from "./Share";
+import CollapsableHeader from './CollapsableHeader';
+import Design from './Design';
+import Fill from './Fill';
+import Share from './Share';
 
 function Form() {
   return (
     <form className="content__form js-form" action="">
       <section className="js-collapsable-container">
-        <CollapsableHeader title="Diseña" />
+        <CollapsableHeader
+          title="Diseña"
+          icon="far fa-object-ungroup form__iconL"
+        />
         <Design />
       </section>
 
       <section className="fill js-collapsable-container collapsable--close">
-        <CollapsableHeader title="Rellena" />
+        <CollapsableHeader title="Rellena" icon="far fa-keyboard form__iconL" />
         <Fill />
       </section>
       <div className="form__border"></div>
 
       <section className="sharecards js-collapsable-container collapsable--close">
-        <CollapsableHeader title="Comparte" />
+        <CollapsableHeader
+          title="Comparte"
+          icon="fas fa-share-alt form__iconL"
+        />
         <Share />
       </section>
       <div className="form__border--share"></div>
