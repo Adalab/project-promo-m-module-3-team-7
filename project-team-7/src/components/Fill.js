74b60ab37f478ce7ed7639835c021bc79de16582
Ana Salamero <asalamerosanz@gmail.com>
2021-06-04 14:06:30 +0200

    added input component

--- project-team-7/src/components/Fill.js
+++ project-team-7/src/components/Fill.js
@@ -1,26 +1,24 @@
+import Input from './Input';
+
 function Fill() {
   return (
     <div className="form__fill__content hidden__container">
-      <label htmlFor="fullname" className="form__fill__label">
-        Nombre completo{" "}
-      </label>
-      <input
+      <Input
+        label="Nombre completo"
         type="text"
         name="name"
         id="fullname"
-        className="form__fill__input js-name"
+        className="js-name"
         placeholder="Ej: Sally Jill"
       />
 
-      <label htmlFor="job" className="form__fill__label">
-        Puesto{" "}
-      </label>
-      <input
+      <Input
+        label="Puesto"
         type="text"
         name="job"
         id="job"
-        className="form__fill__input js-job"
-        placeholder="Ej:Front-end unicorn"
+        className="js-job"
+        placeholder="Ej: Front-end unicorn"
       />
 
       <label className="form__fill__label">Imagen de perfil </label>
@@ -38,47 +36,39 @@ function Fill() {
         <div className="form__fill__photo--preview js__profile-image"></div>
       </div>
 
-      <label htmlFor="email" className="form__fill__label ">
-        Email{" "}
-      </label>
-      <input
+      <Input
+        label="Email"
         type="email"
         name="email"
         id="email"
-        className="form__fill__input js-email"
+        className="js-email"
         placeholder="Ej:sally-hill@gmail.com"
       />
 
-      <label htmlFor="phone" className="form__fill__label">
-        Teléfono{" "}
-      </label>
-      <input
+      <Input
+        label="Teléfono"
         type="tel"
         name="phone"
         id="phone"
-        className="form__fill__input js-phone"
+        className="js-phone"
         placeholder="Ej:555-55-55"
       />
 
-      <label htmlFor="linkedin" className="form__fill__label">
-        Linkedin{" "}
-      </label>
-      <input
+      <Input
+        label="Linkedin"
         type="text"
         name="linkedin"
         id="linkedin"
-        className="form__fill__input js-linkedin"
+        className="js-linkedin"
         placeholder="Ej: sally-jill"
       />
 
-      <label htmlFor="github" className="form__fill__label">
-        Github{" "}
-      </label>
-      <input
+      <Input
+        label="Github"
         type="text"
         name="github"
         id="github"
-        className="form__fill__input js-github"
+        className="js-github"
         placeholder="Ej: @Sally-Jill"
       />
     </div>
