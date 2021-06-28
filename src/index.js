72302c079c2c1095da58aa3dd9c5583a747d86f8
MariaSernaZ <maria.serna.zafra@gmail.com>
2021-06-28 13:05:09 +0200

    Try heroku

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json({ limit: '10mb' }));
-server.set('view engine', 'ejs');
+server.use(express.json({ limit: "10mb" }));
+server.set("view engine", "ejs");
 
 // init express aplication
 const serverPort = process.env.port || 4000;
@@ -14,24 +14,24 @@ server.listen(serverPort, () => {
 });
 
 // static server
-const serverStaticPath = './public';
+const serverStaticPath = "./public";
 server.use(express.static(serverStaticPath));
 
 //database
-const Database = require('better-sqlite3');
-const db = new Database('./src/data/database.db', {
+const Database = require("better-sqlite3");
+const db = new Database("./src/data/database.db", {
   verbose: console.log,
 });
 
-server.get('/card/:id', (req, res) => {
-  const query = db.prepare(`SELECT * FROM users`);
-  const cardData = query.all();
+server.get("/card/:id", (req, res) => {
+  const query = db.prepare(`SELECT * FROM users WHERE id= ?`);
+  const cardData = query.get(req.params.id);
   console.log(cardData);
 
-  res.render('views/card', cardData);
+  res.render("views/card", cardData);
 });
 
-server.post('/card/', (req, res) => {
+server.post("/card/", (req, res) => {
   const query = db.prepare(
     `SELECT * FROM users WHERE palette= ? AND name = ? AND job = ? AND email = ? AND phone = ? AND linkedin = ? AND github = ? AND photo = ?`
   );
@@ -47,48 +47,46 @@ server.post('/card/', (req, res) => {
     req.body.photo
   );
 
-  if (foundUser === undefined) {
-    res.json({ error: 'Completa todos los datos' });
+  let responseError = "";
+  console.log(foundUser);
+
+  if (foundUser.name === "") {
+    response.success = false;
+    responseError = "Missing name";
+  } else if (isNaN(parseInt(foundUser.palette))) {
+    response.success = false;
+    responseError = "Missing palette";
+  } else if (foundUser.job === "") {
+    response.success = false;
+    responseError = "Missing job";
+  } else if (foundUser.email === "") {
+    response.success = false;
+    responseError = "Missing email";
+  } else if (foundUser.phone === "") {
+    response.success = false;
+    responseError = "Missing phone";
+  } else if (foundUser.linkedin === "") {
+    response.success = false;
+    responseError = "Missing linkedin";
+  } else if (foundUser.github === "") {
+    response.success = false;
+    responseError = "Missing github";
+  } else if (foundUser.photo === "") {
+    response.success = false;
+    responseError = "Missing photo";
   } else {
-    res.json({ id: foundUser.id });
+    response.success = true;
+    response.cardURL =
+      "https://awesomeprofilecards.herokuapp.com/card/" +
+      result.lastInsertRowid;
   }
+  res.json({ error: responseError });
 
   console.log(foundUser);
 
-  /*console.log('Peticin en /card');
+  console.log("Peticin en /card");
   console.log(req.body);
   const response = {};
 
-  if (req.body.name === '') {
-    response.success = false;
-    response.error = 'Missing name';
-  } else if (isNaN(parseInt(req.body.palette))) {
-    response.success = false;
-    response.error = 'Missing palette';
-  } else if (req.body.job === '') {
-    response.success = false;
-    response.error = 'Missing job';
-  } else if (req.body.email === '') {
-    response.success = false;
-    response.error = 'Missing email';
-  } else if (req.body.phone === '') {
-    response.success = false;
-    response.error = 'Missing phone';
-  } else if (req.body.linkedin === '') {
-    response.success = false;
-    response.error = 'Missing linkedin';
-  } else if (req.body.github === '') {
-    response.success = false;
-    response.error = 'Missing github';
-  } else if (req.body.photo === '') {
-    response.success = false;
-    response.error = 'Missing photo';
-  } else {
-    response.success = true;
-    response.cardURL = '';
-  }
-
   res.json({ response });
-
-  */
 });
