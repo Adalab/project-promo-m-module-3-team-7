b60b8de8fd07073cccc02ae3167d0c1bad358f69
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-06-28 20:40:16 +0200

    Fix some bugs

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ server.use(express.json({ limit: "10mb" }));
 server.set("view engine", "ejs");
 
 // init express aplication
-const serverPort = process.env.port || 4000;
+const serverPort = process.env.PORT || 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
@@ -26,12 +26,13 @@ const db = new Database("./src/data/database.db", {
 server.get("/card/:id", (req, res) => {
   const query = db.prepare(`SELECT * FROM users WHERE id= ?`);
   const cardData = query.get(req.params.id);
-  console.log(cardData);
+  /*console.log(cardData);*/
 
-  res.render("views/card", cardData);
+  res.render("card", cardData);
 });
 
 server.post("/card/", (req, res) => {
+  /*
   const query = db.prepare(
     `SELECT * FROM users WHERE palette= ? AND name = ? AND job = ? AND email = ? AND phone = ? AND linkedin = ? AND github = ? AND photo = ?`
   );
@@ -46,47 +47,63 @@ server.post("/card/", (req, res) => {
     req.body.github,
     req.body.photo
   );
-
-  let responseError = "";
-  console.log(foundUser);
-
-  if (foundUser.name === "") {
-    response.success = false;
-    responseError = "Missing name";
-  } else if (isNaN(parseInt(foundUser.palette))) {
-    response.success = false;
-    responseError = "Missing palette";
-  } else if (foundUser.job === "") {
-    response.success = false;
-    responseError = "Missing job";
-  } else if (foundUser.email === "") {
-    response.success = false;
-    responseError = "Missing email";
-  } else if (foundUser.phone === "") {
-    response.success = false;
-    responseError = "Missing phone";
-  } else if (foundUser.linkedin === "") {
-    response.success = false;
-    responseError = "Missing linkedin";
-  } else if (foundUser.github === "") {
-    response.success = false;
-    responseError = "Missing github";
-  } else if (foundUser.photo === "") {
-    response.success = false;
-    responseError = "Missing photo";
-  } else {
-    response.success = true;
-    response.cardURL =
-      "https://awesomeprofilecards.herokuapp.com/card/" +
-      result.lastInsertRowid;
-  }
-  res.json({ error: responseError });
-
-  console.log(foundUser);
-
-  console.log("Peticin en /card");
-  console.log(req.body);
+*/
+  /*console.log(req.body);*/
   const response = {};
 
-  res.json({ response });
+  if (req.body.name === "") {
+    response.success = false;
+    response.error = "Missing name";
+  } else if (isNaN(parseInt(req.body.palette))) {
+    response.success = false;
+    response.error = "Missing palette";
+  } else if (req.body.job === "") {
+    response.success = false;
+    response.error = "Missing job";
+  } else if (req.body.email === "") {
+    response.success = false;
+    response.error = "Missing email";
+  } else if (req.body.phone === "") {
+    response.success = false;
+    response.error = "Missing phone";
+  } else if (req.body.linkedin === "") {
+    response.success = false;
+    response.error = "Missing linkedin";
+  } else if (req.body.github === "") {
+    response.success = false;
+    response.error = "Missing github";
+  } else if (req.body.photo === "") {
+    response.success = false;
+    response.error = "Missing photo";
+  } else {
+    // INSERT de los datos en la bbdd
+
+    const insertStmt = db.prepare(
+      "INSERT INTO users (palette, name, job, phone, photo, email, github, linkedin)" +
+        "VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+    );
+
+    const result = insertStmt.run(
+      req.body.palette,
+      req.body.name,
+      req.body.job,
+      req.body.phone,
+      req.body.photo,
+      req.body.email,
+      req.body.github,
+      req.body.linkedin
+    );
+
+    response.success = true;
+    if (req.hostname === "localhost") {
+      response.cardURL =
+        `http://localhost:${serverPort}/card/` + result.lastInsertRowid;
+    } else {
+      response.cardURL =
+        "https://awesomeprofilecards.herokuapp.com/card/" +
+        result.lastInsertRowid;
+    }
+  }
+
+  res.json(response);
 });
